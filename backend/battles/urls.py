c8f7cd11b9b31d3d0e7c128f5b9a50ac59475202
Nathalia <npl@cin.ufpe.br>
2021-05-31 13:48:48 -0300

    fix: url path

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("", HomeView.as_view(), name="home"),
     path("battle/", BattleListView.as_view(), name="battles"),
-    path("battle/<int:pk>", BattleDetailView.as_view(), name="battle-detail"),
+    path("battle/<int:pk>/", BattleDetailView.as_view(), name="battle-detail"),
     path("battle/new/", CreateBattleView.as_view(), name="battle-opponent"),
-    path("battle/<int:pk>/team/new", SelectTeamView.as_view(), name="battle-team-pokemons"),
+    path("battle/<int:pk>/team/new/", SelectTeamView.as_view(), name="battle-team-pokemons"),
 ]
