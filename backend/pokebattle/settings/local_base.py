c9261c5ff512c3bb11082f7e28d94d02e2ec6dfa
Nathalia <npl@cin.ufpe.br>
2021-08-02 14:51:33 -0300

    refactor: celery url worker redis

--- backend/pokebattle/settings/local_base.py
+++ backend/pokebattle/settings/local_base.py
@@ -28,6 +28,8 @@ AUTH_PASSWORD_VALIDATORS = []  # allow easy passwords only on local
 # Celery
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
+CELERY_BROKER_URL = config("REDIS_URL")
+CELERY_RESULT_BACKEND = config("REDIS_URL")
 
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
