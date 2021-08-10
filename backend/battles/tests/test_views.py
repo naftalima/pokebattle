6a0b657f0edc47ef4bd937efa7003d7d0e8bd9b6
Nathalia <npl@cin.ufpe.br>
2021-08-10 15:45:29 -0300

    fix: user auth

--- backend/battles/tests/test_views.py
+++ backend/battles/tests/test_views.py
@@ -3,8 +3,7 @@ from common.utils.tests import TestCaseUtils
 
 class CreateBattleViewTest(TestCaseUtils):
     def test_logged_in_uses_correct_template(self):
-        self.client.login(email=self.user.email, password=self._user_password)
-        response = self.client.get("/battle/new/")
+        response = self.auth_client.get("/battle/new/")
 
         # Check our user is logged in
         self.assertEqual(str(response.context["user"]), self.user.email)
@@ -15,5 +14,6 @@ class CreateBattleViewTest(TestCaseUtils):
         self.assertTemplateUsed(response, "battles/battle-opponent.html")
 
     def test_redirect_if_not_logged_in(self):
-        response = self.client.get("/battle/new/")
+        response = self.auth_client.get("/battle/new/")
         self.assertRedirects(response, "/login/?next=/battle/new/")
+        self.assertEqual(response.status_code, 200)
