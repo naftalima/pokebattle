4cedda68ee0ccdc3b71e5f70b5e7d56f0f764470
Nathalia <npl@cin.ufpe.br>
2021-08-17 17:04:20 -0300

    refactor

--- backend/battles/tests/test_views.py
+++ backend/battles/tests/test_views.py
@@ -12,10 +12,10 @@ class CreateBattleViewTest(TestCaseUtils):
         self.opponent = baker.make("users.User")
 
     def test_logged_in_uses_correct_template(self):
-        response = self.auth_client.get("/battle/new/")
+        response = self.auth_client.get(reverse("battle-opponent"))
 
         # Check our user is logged in
-        self.assertEqual(str(response.context["user"]), self.user.email)
+        self.assertEqual(response.context["user"].email, self.user.email)
         # Check that we got a response "success"
         self.assertEqual(response.status_code, 200)
 
@@ -24,7 +24,7 @@ class CreateBattleViewTest(TestCaseUtils):
 
     def test_redirect_if_not_logged_in(self):
         self.auth_client.logout()
-        response = self.auth_client.get("/battle/new/")
+        response = self.auth_client.get(reverse("battle-opponent"))
         self.assertRedirects(response, "/login/?next=/battle/new/")
         self.assertEqual(response.status_code, 302)
 
@@ -50,6 +50,9 @@ class CreateBattleViewTest(TestCaseUtils):
         response = self.auth_client.post(reverse("battle-opponent"), battle_data)
 
         battle = Battle.objects.filter(creator=self.user, opponent=self.opponent)
+        self.assertEqual(battle[0].creator.email, self.user.email)
+        self.assertEqual(battle[0].opponent.email, self.opponent.email)
+
         self.assertTrue(battle)
 
         self.assertEqual(response.status_code, 302)
@@ -60,6 +63,8 @@ class CreateBattleViewTest(TestCaseUtils):
         }
 
         self.auth_client.post(reverse("battle-opponent"), battle_data)
+
+        self.assertRaisesMessage(ValueError, "ERROR: All fields are required.")
 
         battle = Battle.objects.filter(creator=self.user, opponent=self.opponent)
         self.assertFalse(battle)
