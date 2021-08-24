3bc5ba55be3f2d183afd4883cb5d9d3ae010c5d8
Nathalia <npl@cin.ufpe.br>
2021-08-24 14:54:40 -0300

    feat: LogicBattleTest

--- backend/battles/tests/test_services.py
+++ backend/battles/tests/test_services.py
@@ -1,4 +1,8 @@
+from model_bakery import baker
+
+from battles.models import TeamPokemon
 from battles.services.api_integration import get_pokemon_info
+from battles.services.logic_battle import get_winner
 from battles.services.logic_team_pokemon import (
     check_pokemons_unique,
     check_position_unique,
@@ -39,3 +43,33 @@ class LogicTeamPokemonTest(TestCaseUtils):
         pokemons_data = [get_pokemon_info(pokemon_name) for pokemon_name in pokemon_names]
         is_team_sum_valid = check_team_sum_valid(pokemons_data)
         self.assertFalse(is_team_sum_valid)
+
+
+class LogicBattleTest(TestCaseUtils):
+    def setUp(self):
+        super().setUp()
+        self.creator = self.user
+        self.opponent = baker.make("users.User")
+        self.battle = baker.make("battles.Battle", creator=self.creator, opponent=self.opponent)
+        self.team_creator = baker.make("battles.Team", battle=self.battle, trainer=self.creator)
+        self.team_opponent = baker.make("battles.Team", battle=self.battle, trainer=self.opponent)
+        self.pokemons = [baker.make("pokemons.Pokemon") for i in range(1, 4)]
+
+    def test_get_winner(self):
+        print(self.pokemons)
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[1, 2, 3], team=self.team_creator)
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[3, 2, 1], team=self.team_opponent)
+        winner = get_winner(self.battle)
+        self.assertTrue(winner)
+
+    def test_opponent_wins_in_a_tie(self):
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[1, 2, 3], team=self.team_creator)
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[1, 2, 3], team=self.team_opponent)
+        winner = get_winner(self.battle)
+        self.assertTrue(winner)
+        self.assertEqual(winner, self.opponent)
+
+
+def add_pokemons_to_team(pokemons, positions, team):
+    for pokemon, position in zip(pokemons, positions):
+        TeamPokemon.objects.create(team=team, pokemon=pokemon, order=position)
