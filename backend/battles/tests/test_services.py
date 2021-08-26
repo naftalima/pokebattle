ded9c6ae9dab4a7903886be93527f011e9ca435f
Nathalia <npl@cin.ufpe.br>
2021-08-26 14:48:14 -0300

    Merge branch 'feat/test_email' into feat/test_services

--- backend/battles/tests/test_services.py
+++ backend/battles/tests/test_services.py
@@ -5,10 +5,18 @@ from django.urls import reverse
 
 from model_bakery import baker
 
-from battles.models import Battle
-from battles.services.api_integration import check_pokemons_exists, get_pokemons_data
-from battles.services.logic_battle import get_pokemons  # pylint: disable=import-error
-from battles.services.logic_team_pokemon import check_team_sum_valid
+from battles.models import Battle, TeamPokemon
+from battles.services.api_integration import (
+    check_pokemons_exists,
+    get_pokemon_info,
+    get_pokemons_data,
+)
+from battles.services.logic_battle import get_pokemons, get_winner
+from battles.services.logic_team_pokemon import (
+    check_pokemons_unique,
+    check_position_unique,
+    check_team_sum_valid,
+)
 from battles.utils.format import get_username  # pylint: disable=import-error
 from common.utils.tests import TestCaseUtils
 
@@ -235,3 +243,66 @@ class SendResultMailTest(TestCaseUtils):  # pylint: disable=too-many-instance-at
                 "opponent_pokemon_team": get_pokemons(battle)["opponent"],
             },
         )
+
+
+class LogicTeamPokemonTest(TestCaseUtils):
+    def test_position_unique(self):
+        positions = [1, 2, 3]
+        is_positions_unique = check_position_unique(positions)
+        self.assertTrue(is_positions_unique)
+
+    def test_position_repeated(self):
+        positions = [1, 2, 2]
+        is_positions_unique = check_position_unique(positions)
+        self.assertFalse(is_positions_unique)
+
+    def test_pokemons_unique(self):
+        pokemon_names = ["pikachu", "eevee", "nidorina"]
+        is_pokemons_unique = check_pokemons_unique(pokemon_names)
+        self.assertTrue(is_pokemons_unique)
+
+    def test_pokemons_repeated(self):
+        pokemon_names = ["pikachu", "pikachu", "pikachu"]
+        is_pokemons_unique = check_pokemons_unique(pokemon_names)
+        self.assertFalse(is_pokemons_unique)
+
+    def test_team_sum_valid(self):
+        pokemon_names = ["pikachu", "eevee", "nidorina"]
+        pokemons_data = [get_pokemon_info(pokemon_name) for pokemon_name in pokemon_names]
+        is_team_sum_valid = check_team_sum_valid(pokemons_data)
+        self.assertTrue(is_team_sum_valid)
+
+    def test_team_sum_invalid(self):
+        pokemon_names = ["bulbasaur", "ivysaur", "venusaur"]
+        pokemons_data = [get_pokemon_info(pokemon_name) for pokemon_name in pokemon_names]
+        is_team_sum_valid = check_team_sum_valid(pokemons_data)
+        self.assertFalse(is_team_sum_valid)
+
+
+class LogicBattleTest(TestCaseUtils):
+    def setUp(self):
+        super().setUp()
+        self.creator = self.user
+        self.opponent = baker.make("users.User")
+        self.battle = baker.make("battles.Battle", creator=self.creator, opponent=self.opponent)
+        self.team_creator = baker.make("battles.Team", battle=self.battle, trainer=self.creator)
+        self.team_opponent = baker.make("battles.Team", battle=self.battle, trainer=self.opponent)
+        self.pokemons = [baker.make("pokemons.Pokemon") for i in range(1, 4)]
+
+    def test_get_winner(self):
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[1, 2, 3], team=self.team_creator)
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[3, 2, 1], team=self.team_opponent)
+        winner = get_winner(self.battle)
+        self.assertTrue(winner)
+
+    def test_opponent_wins_in_a_tie(self):
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[1, 2, 3], team=self.team_creator)
+        add_pokemons_to_team(pokemons=self.pokemons, positions=[1, 2, 3], team=self.team_opponent)
+        winner = get_winner(self.battle)
+        self.assertTrue(winner)
+        self.assertEqual(winner, self.opponent)
+
+
+def add_pokemons_to_team(pokemons, positions, team):
+    for pokemon, position in zip(pokemons, positions):
+        TeamPokemon.objects.create(team=team, pokemon=pokemon, order=position)
