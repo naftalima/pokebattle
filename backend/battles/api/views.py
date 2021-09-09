de7667bcf832ff91de6174328a14e87c6fabf5af
Nathalia <npl@cin.ufpe.br>
2021-09-09 10:32:23 -0300

    feat: add permissions

--- backend/battles/api/views.py
+++ backend/battles/api/views.py
@@ -1,13 +1,15 @@
 from django.db.models import Q
 
-from rest_framework import generics
+from rest_framework import generics, permissions
 
+from battles.api.permissions import IsTheTrainerOfTheTeam
 from battles.api.serializers import BattleSerializer, CreateBattleSerializer, SelectTeamSerializer
 from battles.models import Battle, Team
 
 
 class BattleListView(generics.ListAPIView):
     serializer_class = BattleSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         queryset = Battle.objects.filter(
@@ -18,10 +20,12 @@ class BattleListView(generics.ListAPIView):
 
 class CreateBattleView(generics.CreateAPIView):
     serializer_class = CreateBattleSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class SelectTeamView(generics.UpdateAPIView):
     serializer_class = SelectTeamSerializer
+    permission_classes = [IsTheTrainerOfTheTeam]
 
     def get_queryset(self):
         queryset = Team.objects.all()
