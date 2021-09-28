d1e26d35f559a51af4b96efc33615c19a4074dff
Nathalia <npl@cin.ufpe.br>
2021-09-28 19:23:46 -0300

    feat: route battle detail and route battle list

--- frontend/js/App.js
+++ frontend/js/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-// import BattleDetail from './pages/BattleDetail';
+import BattleDetail from './pages/BattleDetail';
 import BattleList from './pages/BattleList';
 import store from './redux/store';
 import SentryBoundary from './utils/SentryBoundary';
@@ -10,8 +11,12 @@ import SentryBoundary from './utils/SentryBoundary';
 const App = () => (
   <SentryBoundary>
     <Provider store={store}>
-      <BattleList />
-      {/* <BattleDetail /> */}
+      <Router>
+        <Switch>
+          <Route component={BattleList} exact path="/v2/battle/" />
+          <Route component={BattleDetail} exact path="/v2/battle/:id" />
+        </Switch>
+      </Router>
     </Provider>
   </SentryBoundary>
 );
