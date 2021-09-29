607792c1192d76097b6af8229645ac86fbdb9373
Nathalia <npl@cin.ufpe.br>
2021-09-29 13:30:29 -0300

    try fix

--- frontend/js/App.js
+++ frontend/js/App.js
@@ -7,7 +7,7 @@ import BattleDetail from './pages/BattleDetail';
 import BattleList from './pages/BattleList';
 import store from './redux/store';
 import SentryBoundary from './utils/SentryBoundary';
-import './pages/Style.scss';
+// import './pages/Style.scss';
 
 const App = () => (
   <SentryBoundary>
