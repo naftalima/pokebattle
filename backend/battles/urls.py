5d984b0765379ee3f32ccfb892d030c169e79da0
Nathalia <npl@cin.ufpe.br>
2021-10-07 10:42:00 -0300

    feat: create battle urll

--- backend/battles/urls.py
+++ backend/battles/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("v2/battle/<int:pk>", BaseView.as_view(), name="v2_battle_detail"),
     path("v2/battle/", BaseView.as_view(), name="v2_battle_list"),
+    path("v2/battle/new/", BaseView.as_view(), name="v2_battle-opponent"),
     path("battle/", BattleListView.as_view(), name="battles"),
     path("battle/<int:pk>/", BattleDetailView.as_view(), name="battle-detail"),
     path("battle/new/", CreateBattleView.as_view(), name="battle-opponent"),
