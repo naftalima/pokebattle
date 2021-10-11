9d94718e860572563030a588910f5e7c4ef4baa7
Nathalia <npl@cin.ufpe.br>
2021-10-11 10:43:45 -0300

    feat: embedding any expression in curly braces so we could inline the map() result

--- frontend/js/components/Battles.js
+++ frontend/js/components/Battles.js
@@ -3,16 +3,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Battles({ battles }) {
-  const battleList = battles.map((battle) => {
-    return (
-      <tr key={battle.id} className="battle-id-btn">
-        <Link to={{ pathname: `/v2/battle/${battle.id}` }}>Battle #{battle.id}</Link>
-      </tr>
-    );
-  });
   return (
     <table>
-      <td>{battleList}</td>
+      <td>
+        {battles.map((battle) => (
+          <tr key={battle.id} className="battle-id-btn">
+            <Link to={{ pathname: `/v2/battle/${battle.id}` }}>Battle #{battle.id}</Link>
+          </tr>
+        ))}
+      </td>
     </table>
   );
 }
